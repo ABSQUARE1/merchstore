6c4a79b92795d517693a2127e3b9f551dfce580f
ABSQUARE1 <yusufolatuji53@gmail.com>
2023-05-22 13:20:01 +0000

    modified data

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,70 +41,7 @@ resource "aws_instance" "my-nginx-server4" {
   }
 }
 
-# Create EC2 Instance - Ubuntu 20.04 for nginx
-# resource "aws_instance" "my-nginx-server5" {
-#   ami                    = "ami-0aa2b7722dc1b5612"
-#   instance_type          = "t2.micro"
-#   availability_zone      = "us-east-1a"
-#   key_name               = "memorykeypair"
-#   vpc_security_group_ids = ["${aws_security_group.my_asg.id}"]
-  
-#   # user_data : render the template
-#   user_data     = base64encode("${data.template_file.user_data1.rendered}")
 
-#   tags = {
-#     "Name" = "Ubuntu Nginx server 2"
-#   }
-# }
-
-
-# # Create a VPC
-# resource "aws_vpc" "my_test_vpc1" {
-#   cidr_block           = "10.0.0.0/16"
-#   enable_dns_hostnames = true
-#   enable_dns_support   = true
-
-#   tags = {
-#     name = "dev"
-#   }
-# }
-
-# creating a subnet
-resource "aws_subnet" "my_test_vpc1_PublicSubnet1" {
-  vpc_id = aws_vpc.my_test_vpc1.id
-  cidr_block              = "10.0.0.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
-
-  tags = {
-    Name = "Public Subnet 1"
-  }
-}
-
-
-# create internet gateway
-resource "aws_internet_gateway" "my_test_vpc1_Internetgateway" {
-  vpc_id = aws_vpc.my_test_vpc1.id
-
-  tags = {
-    Name = "Gateway"
-  }
-}
-# created a  route table to accept traffic from anywhere on the internet
-resource "aws_route_table" "vpc_route" {
-  vpc_id = aws_vpc.my_test_vpc1.id
-
-  tags = {
-    Name = "public-route"
-  }
-}
-
-resource "aws_route" "route-inline" {
-  route_table_id         = aws_route_table.vpc_route.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.my_test_vpc1_Internetgateway.id
-
-}
 
 
 resource "aws_security_group" "my_asg" {
