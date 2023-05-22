f0438372a9c8939f30e2bae76d70f35107b03a8d
ABSQUARE1 <yusufolatuji53@gmail.com>
2023-05-22 13:11:41 +0000

    modified data

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,24 +71,13 @@ resource "aws_vpc" "my_test_vpc1" {
 
 # creating a subnet
 resource "aws_subnet" "my_test_vpc1_PublicSubnet1" {
-  vpc_id                  = aws_vpc.my_test_vpc1.id
+  vpc_id = aws_vpc.my_test_vpc1.id
   cidr_block              = "10.0.0.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1a"
 
   tags = {
     Name = "Public Subnet 1"
-  }
-}
-# creating a subnet
-resource "aws_subnet" "my_test_vpc1_PublicSubnet2" {
-  vpc_id                  = aws_vpc.my_test_vpc1.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
-
-  tags = {
-    Name = "Public Subnet 2"
   }
 }
 
